8bb18e002116e97fc071ba0e7ba25f476ad55db0
kazifarhanuddin <kazifarhan199@gmail.com>
2019-04-18 03:47:10 +0530

    Added tinymce for post body field & added same stylling for post in header

--- Colab_site/urls.py
+++ Colab_site/urls.py
@@ -22,8 +22,10 @@ urlpatterns = [
     path('', include('Projects.urls')),
     path('admin/', admin.site.urls),  
     path('accounts/', include('Accounts.urls')),
-
+    
+    
+    
+    re_path(r'^tinymce/', include('tinymce.urls')),
     re_path(r'static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     re_path(r'media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
-
 ]
