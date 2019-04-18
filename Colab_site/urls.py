c82d9189363b3c687bef9f7b6de06aa712609181
kazifarhanuddin <kazifarhan199@gmail.com>
2019-04-18 19:28:37 +0530

    Added static files

--- Colab_site/urls.py
+++ Colab_site/urls.py
@@ -23,8 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),  
     path('accounts/', include('Accounts.urls')),
     
-    
-    
     re_path(r'^tinymce/', include('tinymce.urls')),
     re_path(r'static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     re_path(r'media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
